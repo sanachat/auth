16e57b4d738192d31666b35de1001ddcf22ce81b
Tracy McGrady <895121113@qq.com>
2017-08-03 16:55:46 +0800

    用户实现类和接口

---
+++ auth-dao/src/main/java/com.hzit/dao/UserInfoDao.java
@@ -0,0 +1,19 @@
+package com.hzit.dao;
+
+import com.hzit.entity.UserInfo;
+
+import java.util.List;
+
+/**
+ * Created by Tracy McGrady on 2017/8/3.
+ */
+public interface UserInfoDao
+{
+    public List<UserInfo> findUser();
+
+    public int addUser(UserInfo userInfo);
+
+    public int updateUser(UserInfo userInfo);
+
+    public int deleteUser(String uid);
+}
