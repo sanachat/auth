b2ee0cbc6362e0ad3ff118fa29e7707e964ddb33
xiaowan1 <1335422698@QQ.com>
2017-08-03 16:55:42 +0800

    实体类

---
+++ auth-dao/src/main/java/com.hzit/entity/UrInfo.java
@@ -0,0 +1,41 @@
+package com.hzit.entity;
+
+/**
+ * 　　  　  　           \\\|///
+ * 　　　 　  　   \\　.-.-　//
+ * 　　　　　  　(　 .@.@　)
+ * +-------oOOo-----( _ )-----oOOo--------------------------------------------+
+ * |　@author 万家明　　　　　                                                                 　|
+ * |　@author 江西财经大学                                                  |
+ * |　@create 2017年08月03日 - 16:51
+ * |  @description   角色用户关联表
+ * +---------------------------------Oooo---------------------------------------+
+ */
+public class UrInfo {
+    private int uid;
+    private int rid;
+
+    public int getRid() {
+        return rid;
+    }
+
+    public void setRid(int rid) {
+        this.rid = rid;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    @Override
+    public String toString() {
+        return "UrInfo{" +
+                "uid=" + uid +
+                ", rid=" + rid +
+                '}';
+    }
+}
