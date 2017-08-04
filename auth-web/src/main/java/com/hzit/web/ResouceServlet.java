7649d35f96f1228abbb340bcf5339df7e2962eae
sanachat <1010773421@qq.com>
2017-08-04 16:01:45 +0800

    1

--- auth-web/src/main/java/com/hzit/web/ResouceServlet.java
+++ auth-web/src/main/java/com/hzit/web/ResouceServlet.java
@@ -1,19 +1,27 @@
 package com.hzit.web;
 
+import com.hzit.dao.ResourcesDao;
+import com.hzit.dao.SqlSessionHelper;
+import com.hzit.entity.Resources;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/4.
  */
 @WebServlet(name = "ResouceServlet", value="/resource")
 public class ResouceServlet extends HttpServlet {
-    
+    ResourcesDao dao= SqlSessionHelper.getSqlSession().getMapper(ResourcesDao.class);
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Resources> allresource=dao.findAll();
+        request.setAttribute("allresource",allresource);
+        request.getRequestDispatcher("resource.jsp").forward(request,response);
 
     }
 
